228d1bdeef0a15cc5909afbe9e2c2f9acda317c0
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-21 09:26:21 +0900

    hello and logger

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-const msg: string = "hello world";
-console.log(msg);
+import connect from "connect";
+import { ServerResponse } from "http";
+
+const logger = (
+  req: connect.IncomingMessage,
+  _: ServerResponse<connect.IncomingMessage>,
+  next: connect.NextFunction
+) => {
+  console.log("%s, %s", req.method, req.url);
+  next();
+};
+
+const hello = (
+  _: connect.IncomingMessage,
+  res: ServerResponse<connect.IncomingMessage>
+) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+};
+
+const app = connect();
+app.use(logger).use(hello).listen(3000);
