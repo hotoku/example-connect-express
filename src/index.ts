06c73254717a9852cfbef74edb22d9e4a831ee1d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-22 03:16:55 +0900

    express

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,8 @@
-import connect, {
-  ErrorHandleFunction,
-  HandleFunction,
-  IncomingMessage,
-} from "connect";
-import { ServerResponse } from "http";
-import { createLogger } from "./logger";
+import express from "express";
 
-const hello: HandleFunction = (
-  _: IncomingMessage,
-  res: ServerResponse<IncomingMessage>
-): void => {
-  throw "hoge";
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-};
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-const env = process.env.NODE_ENV || "development";
-
-const myErrorHandler: ErrorHandleFunction = (
-  err: any,
-  req: IncomingMessage,
-  res: ServerResponse<IncomingMessage>,
-  next: any
-): void => {
-  res.statusCode = 500;
-  switch (env) {
-    case "development":
-      console.error("Error:");
-      console.error(err);
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(err));
-      break;
-    default:
-      res.end("Server error");
-  }
-};
-
-const app = connect();
-app
-  .use(createLogger(":hoge :url :method"))
-  .use(hello)
-  .use(myErrorHandler)
-  .listen(3000);
+app.listen(3000);
